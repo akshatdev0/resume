ca21821fe6acebe2ea5b9a39d7971bd9256a6e62
Akshat Nigam <akshat.nigam15@gmail.com>
2026-02-09 16:39:39 +0530

    feat(github): Setup for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://akshatdev0.github.io',
+  base: '/resume',
   vite: {
       plugins: [tailwindcss()],
 	},
-
   integrations: [react()],
 });
